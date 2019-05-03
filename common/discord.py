f5677ec0c92f8883d171aeacbebd0bfc1bac2bae
Copetan <caleb.in.acton@gmail.com>
2019-05-02 20:51:24 -0700

    some code changes idk ksjdngdjng

--- common/discord.py
+++ common/discord.py
@@ -1,26 +1,18 @@
 import discord
+import re
+
 from config.discord import *
 
 
 class DiscordHelper(object):
     @staticmethod
     def filter_content(message: discord.Message):
-        # Remove bot mentions from messages or replace mentions with names
+        # Remove bot mentions from messages
         filtered_content = message.content
         for mention in message.mentions:
             if mention.id == DISCORD_USER_ID:
-                replace_id = mention.id
-                replace_tag = "<@%s>" % replace_id
-                filtered_content = filtered_content.replace(replace_tag, '')
-            else:
-                try:
-                    if mention.nick is not None:
-                        replace_name = mention.nick
-                    else:
-                        replace_name = mention.name
-                except AttributeError:
-                    replace_name = mention.name
-                replace_id = mention.id
-                replace_tag = "<@%s>" % replace_id
-                filtered_content = filtered_content.replace(replace_tag, replace_name)
+                self_id = mention.id
+                replace_self = "<@%s>" % self_id
+                filtered_content = re.sub(replace_self, '', filtered_content)
+                filtered_content = filtered_content.strip()
         return filtered_content
