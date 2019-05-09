64464627f2a4a38a48a74ae9053adf209fdd615c
Copetan <copetanbird@gmail.com>
2019-05-09 13:49:38 -0700

    adding and restructuring a bit

--- common/discord.py
+++ common/discord.py
@@ -7,10 +7,7 @@ class DiscordHelper(object):
     def filter_content(message: discord.Message):
         # Remove mentions and emails from messages
         filtered_content = message.content
-        for mention in message.mentions:
-            self_id = mention.id
-            replace_self = "<@%s>" % self_id
-            filtered_content = filtered_content.replace(replace_self, '')
-        filtered_content = re.sub(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', '', filtered_content)
+        filtered_content = re.sub(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)|'
+                                  r'(<@!?\d*>)', '', filtered_content)
         filtered_content = filtered_content.strip()
         return filtered_content
