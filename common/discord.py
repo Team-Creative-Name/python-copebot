23d1c3de362bfa8aff2103c0fad18db9a77397a9
Copetan <caleb.in.acton@gmail.com>
2019-05-16 11:00:28 -0700

    very minor addition

--- common/discord.py
+++ common/discord.py
@@ -9,5 +9,6 @@ class DiscordHelper(object):
         filtered_content = message.content
         filtered_content = re.sub(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)|'
                                   r'(<@!?\d*>)', '', filtered_content)
+        filtered_content = re.sub(r' +', ' ', filtered_content)
         filtered_content = filtered_content.strip()
         return filtered_content
