1f1fb4be44b7c8e5434001d43e62c013d8883a7b
Copetan <14883143+Copetan@users.noreply.github.com>
2019-10-03 21:10:06 -0700

    minor database capture changes

--- common/discord.py
+++ common/discord.py
@@ -8,7 +8,9 @@ class DiscordHelper(object):
         # Remove mentions and emails from messages
         filtered_content = message.content
         filtered_content = re.sub(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)|'
-                                  r'(<@!?\d*>)', '', filtered_content)
+                                  r'(<@[!&]?\d+>)|'
+                                  r'(<#\d+>)|'
+                                  r'(\bowoh\b)|(\bowob\b)', '', filtered_content)
         filtered_content = re.sub(r' +', ' ', filtered_content)
         filtered_content = filtered_content.strip()
         return filtered_content
