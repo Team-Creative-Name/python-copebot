8c6b49fa617f8cc150f11bb7db8aed7edded56c8
Copetan <14883143+Copetan@users.noreply.github.com>
2020-04-13 16:06:04 -0700

    adding a bit of commenting

--- common/capture_filter.py
+++ common/capture_filter.py
@@ -7,14 +7,17 @@ class MessageFilter(object):
 	@staticmethod
 	def filter_content(message: discord.Message):
 		filtered_content = message.content
-		# Remove unwanted words from messages
+		# Prevents words (declared under bot_config.py) from being captured into the bot's database
 		filtered_content = re.sub(BLOCK_PHRASE_ALL, '', filtered_content, flags=re.IGNORECASE)
+		# Prevents groups of regional indicator emojis of size 2 or larger from being captured
+		# (breaks the bot)
 		filtered_content = re.sub(r'([🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿]{2,})', '', filtered_content)
-		# Remove mentions and emails from messages
+		# Prevents mentions and emails (plus some other things) from being captured
 		filtered_content = re.sub(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)|'
 		                          r'(<@[!&]?\d+>)|'
 		                          r'(<#\d+>)|'
 		                          r'(\bowoh\b)|(\bowob\b)', '', filtered_content)
+		# Shortens groups of more than one whitespace character to a single whitespace character
 		filtered_content = re.sub(r' +', ' ', filtered_content)
 		filtered_content = filtered_content.strip()
 		return filtered_content
